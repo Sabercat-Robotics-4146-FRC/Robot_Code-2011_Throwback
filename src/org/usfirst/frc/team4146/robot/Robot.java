6d5e680cf1af41df23f77326a8c5d16dcc24200b
Eli Arnold <sabercatrobotics@gmail.com>
2019-01-08 17:57:01 -0700

    Rumble and experimental pixy code

--- src/org/usfirst/frc/team4146/robot/Robot.java
+++ src/org/usfirst/frc/team4146/robot/Robot.java
@@ -1,9 +1,16 @@
 package org.usfirst.frc.team4146.robot;
 
 import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.I2C.Port;
 
 public class Robot extends SampleRobot {
-
+	int addr = 84;
+	
+	Controller controller;
+	Timer timer;
+	
+	I2C pixy;
 	public Robot() {
 		
 	}
@@ -14,7 +21,10 @@ public class Robot extends SampleRobot {
 	 */
 	@Override
 	public void robotInit() {
+		controller = new Controller(0);
+		timer = new Timer();
 		
+		pixy = new I2C(I2C.Port.kOnboard, 84);
 	}
 
 	/**
@@ -31,7 +41,32 @@ public class Robot extends SampleRobot {
 	 */
 	@Override
 	public void operatorControl() {
+//		boolean buttonPressedFlag = false;
+		byte[] ary = new byte[14];
 		
+		while (isOperatorControl() && isEnabled()) {
+			timer.update();
+//			controller.updateRumbleBuzz(timer.getDT());
+//			
+//			if(controller.getButtonA() && !buttonPressedFlag) {
+//				System.out.println("Button Pressed and Allowed.");
+//				buttonPressedFlag = true;
+//				controller.setRumbleBuzz(5, 0.25, 0.125);
+//				//controller.setRumbleBuzz(5, 3.0, 3.0);
+//			}
+//			if(!controller.getButtonA()) {
+//				buttonPressedFlag = false;
+//			}
+			
+			pixy.read(addr, 14, ary);
+			
+//			System.out.println("Here: ");
+			for( int i = 0; i < 14; i++ ) {
+//				System.out.println(i + ": \t" + ary[i]);
+				Dashboard.send("THis thing", ary[0]);
+			}
+			System.out.println();
+		}
 	}
 
 	/**
